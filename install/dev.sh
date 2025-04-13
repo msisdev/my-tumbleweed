bf92ebe8cf33e57220256faac8b5cba307abf4e0
msisdev <dmg04158@gmail.com>
2025-04-13 11:22:49 +0900

    browser

--- install/dev.sh
+++ install/dev.sh
@@ -5,10 +5,4 @@ packages=(
     "git"
 )
 
-sudo zypper in "${packages[@]}"
-
-# github-desktop
-xdg-open https://github.com/shiftkey/desktop?tab=readme-ov-file#opensuse
-
-# vscode
-xdg-open https://code.visualstudio.com/docs/setup/linux#_opensuse-and-slebased-distributions
+sudo zypper in -y "${packages[@]}"
