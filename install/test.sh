058b3615b0523d364a4d3a4d467510c0ccee6421
msisdev <dmg04158@gmail.com>
2025-04-13 11:34:13 +0900

    rustup

--- install/test.sh
+++ install/test.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-packages=(
-    "gh"
-    "git"
-    "go"
-    "rustup"
-)
-
-sudo zypper in -y "${packages[@]}"
-
 # rustup https://www.rust-lang.org/tools/install
 echo "rustup"
 echo "  echo '#rustup' >> ~/.bashrc"
 echo "  echo 'export PATH=\"\$HOME/.cargo/bin:\$PATH\' >> ~/.bashrc"
 echo ""
-
