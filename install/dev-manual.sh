bd65f8ce4464d1ce11d2d6a3d493e2e0857d4154
msisdev <dmg04158@gmail.com>
2025-04-13 22:32:53 +0900

    fonts

--- install/dev-manual.sh
+++ install/dev-manual.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 packages=(
+  "font-noto-sans-kr https://fonts.google.com/noto/specimen/Noto+Sans+KR"
+  "font-noto-serif-kr https://fonts.google.com/noto/specimen/Noto+Serif+KR"
+  "font-oxygen-mono https://fonts.google.com/specimen/Oxygen+Mono"
   "github-desktop https://github.com/shiftkey/desktop?tab=readme-ov-file#opensuse"
   "vscode https://code.visualstudio.com/docs/setup/linux#_opensuse-and-slebased-distributions"
 )
