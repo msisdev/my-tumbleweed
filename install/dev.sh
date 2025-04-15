4b98c9c86250bb008227c38407dce2f153ea3ba1
msisdev <dmg04158@gmail.com>
2025-04-15 09:56:05 +0900

    Update dev.sh

--- install/dev.sh
+++ install/dev.sh
@@ -6,12 +6,17 @@ packages=(
     "gh"
     "git"
     "go"
+    "libopenssl-devel"
     "rustup"
 )
 
 sudo zypper in -y "${packages[@]}"
 
+# bun https://bun.sh/docs/installation
+curl -fsSL https://bun.sh/install | bash
+
 # rustup https://www.rust-lang.org/tools/install
 echo "🧰 rustup"
 rustup default stable
 
+cargo install cargo-generate
