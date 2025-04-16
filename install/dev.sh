e7f686faf16796844b984c672b803ad8d3073f0e
msisdev <dmg04158@gmail.com>
2025-04-16 20:24:39 +0900

    node

--- install/dev.sh
+++ install/dev.sh
@@ -12,10 +12,17 @@ packages=(
 
 sudo zypper in -y "${packages[@]}"
 
-# bun https://bun.sh/docs/installation
+
+# bun       https://bun.sh/docs/installation
 curl -fsSL https://bun.sh/install | bash
 
-# rustup https://www.rust-lang.org/tools/install
+# node      https://nodejs.org/en/download
+curl -o- https://fnm.vercel.app/install | bash
+fnm install 22
+node -v
+npm -v
+
+# rustup    https://www.rust-lang.org/tools/install
 echo "🧰 rustup"
 rustup default stable
 
