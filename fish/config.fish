a7453b4832e1ed4e191d263785b498cc70f40e51
msisdev <dmg04158@gmail.com>
2025-05-10 13:34:58 +0900

    backing up...

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    fastfetch
+end
+
+fish_add_path $HOME/go/bin
--- install/fish.sh
+++ install/fish.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # fish https://fishshell.com/
-sudo zypper in -y fish
+# sudo zypper in -y fish
 # bc curl-fish-completion fish fwupd-fish-completion gh-fish-completion libpcre2-32-0
 
-fish
+# fish
 
 # oh-my-fish https://github.com/oh-my-fish/oh-my-fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
@@ -18,4 +18,4 @@ sudo zypper in -y fd    # https://github.com/sharkdp/fd
 sudo zypper in -y bat   # https://github.com/sharkdp/bat
 fisher install PatrickF1/fzf.fish
 
-# fish_add_path 
+# fish_add_path
